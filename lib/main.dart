dd1a965c5dc9b5c492bee7aaa7c4c7a0b0d3bf13
Malik Hammad <hamad.aslam7889@gmail.com>
2021-09-05 18:04:49 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,182 @@
+// import 'dart:convert';
+
+// import 'package:flutter/material.dart';
+// import 'package:flutter_php/details.dart';
+// import 'package:http/http.dart' as http;
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter + php',
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData(
+//         primarySwatch: Colors.red,
+//       ),
+//       home: HomePage(),
+//     );
+//   }
+// }
+
+// class HomePage extends StatefulWidget {
+//   const HomePage({Key key}) : super(key: key);
+
+//   @override
+//   _HomePageState createState() => _HomePageState();
+// }
+
+// class _HomePageState extends State<HomePage> {
+//   Future<List> getData() async {
+//     final response = await http.get("http://localhost/myfolder/getdata.php");
+//     return json.decode(response.body);
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Flutter + php"),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () {},
+//         child: Icon(Icons.add),
+//       ),
+//       body: FutureBuilder<List>(
+//         future: getData(),
+//         builder: (context, snapshot) {
+//           if (snapshot.hasError) {
+//             print("error");
+//           }
+//           if (snapshot.hasData) {
+//             return Items(list: snapshot.data);
+//           } else {
+//             return CircularProgressIndicator();
+//           }
+//         },
+//       ),
+//     );
+//   }
+// }
+// import 'dart:async';
+// import 'dart:convert';
+
+// import 'package:flutter/material.dart';
+// import 'package:http/http.dart' as http;
+
+// Future<Album> fetchAlbum() async {
+//   final response = await http
+//       .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+
+//   if (response.statusCode == 200) {
+//     // If the server did return a 200 OK response,
+//     // then parse the JSON.
+//     return Album.fromJson(jsonDecode(response.body));
+//   } else {
+//     // If the server did not return a 200 OK response,
+//     // then throw an exception.
+//     throw Exception('Failed to load album');
+//   }
+// }
+
+// class Album {
+//   final int userId;
+//   final int id;
+//   final String title;
+
+//   Album({
+//     @required this.userId,
+//     @required this.id,
+//     @required this.title,
+//   });
+
+//   factory Album.fromJson(Map<String, dynamic> json) {
+//     return Album(
+//       userId: json['userId'],
+//       id: json['id'],
+//       title: json['title'],
+//     );
+//   }
+// }
+
+// void main() => runApp(const MyApp());
+
+// class MyApp extends StatefulWidget {
+//   const MyApp({Key key}) : super(key: key);
+
+//   @override
+//   _MyAppState createState() => _MyAppState();
+// }
+
+// class _MyAppState extends State<MyApp> {
+//   Future<Album> futureAlbum;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     futureAlbum = fetchAlbum();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Fetch Data Example',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text('Fetch Data Example'),
+//         ),
+//         body: Center(
+//           child: FutureBuilder<Album>(
+//             future: futureAlbum,
+//             builder: (context, snapshot) {
+//               if (snapshot.hasData) {
+//                 return Text(snapshot.data.title);
+//               } else if (snapshot.hasError) {
+//                 return Text('${snapshot.error}');
+//               }
+
+//               // By default, show a loading spinner.
+//               return const CircularProgressIndicator();
+//             },
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// class Items extends StatelessWidget {
+//   final List list;
+
+//   const Items({Key key, @required this.list}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return
+//   }
+// }
+
+import 'package:flutter/material.dart';
+import 'package:flutter_php/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
---
+++ lib/widget/main_widget.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_php/widget/weather_tile.dart';
+
+class MainWidget extends StatelessWidget {
+  final String location;
+  final double temp;
+  final double tempMin;
+  final double tempMax;
+  final String weather;
+  final int humidity;
+  final double windSpeed;
+  const MainWidget({
+    Key key,
+    this.humidity,
+    this.location,
+    this.temp,
+    this.tempMax,
+    this.tempMin,
+    this.weather,
+    this.windSpeed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: MediaQuery.of(context).size.height / 2,
+          width: MediaQuery.of(context).size.width,
+          color: Color(0xfff1f1f1),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "${location.toString()}",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w900),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10, bottom: 10),
+                child: Text(
+                  "${temp.toInt().toString()}°",
+                  style: TextStyle(
+                      color: Colors.purple,
+                      fontSize: 40,
+                      fontWeight: FontWeight.w900),
+                ),
+              ),
+              Text(
+                "High of ${tempMax.toInt().toString()}°, Low of ${tempMin.toInt().toString()}°",
+                style: TextStyle(
+                    fontSize: 14.0,
+                    fontWeight: FontWeight.w600,
+                    color: Color(0xff9e9e9e)),
+              )
+            ],
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: ListView(
+              children: [
+                WeatherTile(
+                    icn: Icons.thermostat_outlined,
+                    subtitle: "${temp.toInt().toString()}°",
+                    title: "Temperature"),
+                WeatherTile(
+                    icn: Icons.filter_drama_outlined,
+                    subtitle: "${weather.toString()}",
+                    title: "Weather"),
+                WeatherTile(
+                    icn: Icons.wb_sunny,
+                    subtitle: "${humidity.toString()}%",
+                    title: "Humidity"),
+                WeatherTile(
+                    icn: Icons.waves_outlined,
+                    subtitle: "${windSpeed.toInt().toString()} MPH",
+                    title: "Wind Speed"),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
